c8e363abea4c03c5ead1d94d783d2c8b198f6271
Boris Maslakov <b.maslakov@gmail.com>
2017-08-25 18:28:21 +0300

    add DialpadView

---
+++ app/src/main/java/io/uuddlrlrba/dialpadview/MainActivity.java
@@ -0,0 +1,13 @@
+package io.uuddlrlrba.dialpadview;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dialpad_view);
+    }
+}
