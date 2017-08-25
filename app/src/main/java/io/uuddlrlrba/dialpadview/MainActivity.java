967a5bef00203574365550df17d6193dcadbd05a
Boris Maslakov <b.maslakov@gmail.com>
2017-08-25 18:33:05 +0300

    use support library

--- app/src/main/java/io/uuddlrlrba/dialpadview/MainActivity.java
+++ app/src/main/java/io/uuddlrlrba/dialpadview/MainActivity.java
@@ -1,10 +1,9 @@
 package io.uuddlrlrba.dialpadview;
 
-import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
